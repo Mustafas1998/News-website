4eb96feef8c2d8f2600a6a36fb1e50ea94a1da35
Mustafas1998 <ms6534126@gmail.com>
2024-03-17 17:55:11 +0200

    JQuery

--- backend/display.php
+++ backend/display.php
@@ -4,6 +4,11 @@ include("connection.php");
 $sql = "SELECT * FROM news";
 $result = $mysqli->query($sql);
 
+if ($result === false) {
+    echo "Error: " . $mysqli->error;
+    exit;
+}
+
 $news = array();
 
 if ($result->num_rows > 0) {
@@ -14,6 +19,13 @@ if ($result->num_rows > 0) {
 
 header('Content-Type: application/json');
 echo json_encode($news);
+
+$mysqli->close();
+?>;
+
+
+
+
 
 
 
